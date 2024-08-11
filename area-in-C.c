c220f57ad24be5f42f24562b7358b7fbcfa22b97
Abhishek Singh <ilabhishek2023@gmail.com>
2024-08-11 16:17:36 +0530

    Create area-in-C.c

---
+++ area-in-C.c
@@ -0,0 +1,20 @@
+# include<stdio.h>
+
+int main() {
+    int l;
+    int b;
+    int a;
+
+    printf("Enter Length : ");
+    scanf("%d",&l);
+
+    printf("Enter Breadth : ");
+    scanf("%d",&b);
+
+    a = l*b;
+    printf("%d",a);
+
+
+
+    return 0;
+}
